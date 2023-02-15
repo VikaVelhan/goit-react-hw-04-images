f4ab789a03c1843273617ee21e18863e1025a3c0
VikaVelhan <velganvika@gmail.com>
2023-02-15 17:51:06 +0200

    update

--- src/src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,41 +1,14 @@
-//import { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import Modal from '../Modal/Modal';
 
 import css from './ImageGalleryItem.module.css';
 
-export default function ImageGalleryItem({ image }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleToggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-  const { webformatURL, largeImageURL } = image;
-  return (
-    <>
-      <li onClick={handleToggleModal} className={css.ImageGalleryItem}>
-        <img
-          src={webformatURL}
-          alt="img"
-          className={css.ImageGalleryItemImage}
-        />
-      </li>
-      {isModalOpen && (
-        <Modal onClose={handleToggleModal} largeImg={largeImageURL} />
-      )}
-    </>
-  );
-}
-
-ImageGalleryItem.propTypes = {
-  image: PropTypes.object,
-};
-
-/*class oldImageGalleryItem extends Component {
+class ImageGalleryItem extends Component {
   state = {
     isModalOpen: false,
   };
+
   handleToggleModal = () => {
     this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
   };
@@ -58,7 +31,9 @@ ImageGalleryItem.propTypes = {
     );
   }
 }
+
 ImageGalleryItem.propTypes = {
   image: PropTypes.object,
 };
-export default ImageGalleryItem;*/
+
+export default ImageGalleryItem;
